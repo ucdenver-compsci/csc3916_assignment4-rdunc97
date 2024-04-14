960428cbde5135e68fb04794000668e7fa537122
RyanD <ryan.a.duncan@ucdenver.edu>
2024-04-13 18:41:28 -0600

    update

--- server.js
+++ server.js
@@ -107,6 +107,8 @@ router.post('/reviews',function (req,res) {
         });
     }
 });
+
+
 router.get('reviews/id', function(req,res) {
     if (!req.body.username || !req.body.movieid || !req.body.review || !req.body.rating) {
         return res.json({ success: false, message: 'incomplete review'});
@@ -118,6 +120,9 @@ router.get('reviews/id', function(req,res) {
         res.json ({movieid: movieOut.movieid, username: movieOut.username, rating: movieOut.rating, review: movieOut.review});
     })
 
+
+
+//aggregate reviews
     if (req.query.reviews == True) {
         Order.aggregate([
             {
@@ -141,7 +146,9 @@ router.get('reviews/id', function(req,res) {
     }
 });
 
-//movies route
+
+
+//movies route get post put delete 
 router.route('/movies') 
     .get(authJwtController.isAuthenticated, (req,res) => {
         Movie.find(function(err,movies){
@@ -202,99 +209,6 @@ router.route('/movies')
         // Returns a message stating that the HTTP method is unsupported.
         res.status(405).send({ message: 'HTTP method not supported.' });
     });
-
-
-// //movies route 
-// router.post('/movies', authJwtController.isAuthenticated, function(req, res){
-//     if (!req.body.title || !req.body.releaseDate || !req.body.genre || !req.body.actors) {
-//         res.status(400).json({
-//             success: false,
-//             msg: "provide title, name, genre and 3 actors."
-//         });
-//     }
-//         else{
-//             var movie = new Movie();
-//             movie.title = req.body.title;
-//             movie.releaseDate = req.body.releaseDate;
-//             movie.genre = req.body.genre;
-//             movie.actors = req.body.actors;
-
-//             movie.save(function(err){
-//                 if (err){
-//                     res.status(401).json({success: false, msg: "couldn't save movie."});
-//                 }
-//                 else{
-//                     res.json({success: true, msg: "Movie saved successfully."});
-//                 }
-//             });
-//         }
-// });
-// router.get('/movies', authJwtController.isAuthenticated, function(req,res){
-//     Movie.find({}, function(err, movies){
-//         if (err) {
-//             return res.status(500).send(err);
-//         }else{
-//         res.json(movies);
-//         }
-//     });
-// });
-// router.get('/movies/:movieParameter',authJwtController.isAuthenticated, function(req, res){
-//     Movie.findOne({title: req.params.movieParameter}, function(err, movie){
-//         if (err) res.status(500).send(err);
-//         else if (!movie) res.status(404).json({msg:"Movie not found."});
-//         else res.json(movie);
-//     });
-// });
-// router.put('/movies/:movieParameter', authJwtController.isAuthenticated, function(req, res){
-//     Movie.findOneAndUpdate({title: req.params.movieParameter}, req.body,{new: true},function(err, movie){
-//         if(err) res.status(500).send(err);
-//         else if (!movie) res.status(404).json({msg: "Movie not found."});
-//         else res.json(movie);
-//     });
-// });
-// router.delete('/movies/:movieParameter', authJwtController.isAuthenticated, function(req, res){
-//     Movie.findOneAndDelete({title: req.params.movieParameter}, function(err, movie){
-//         if(err) res.status(500).send(err);
-//         else if (!movie) res.status(500).json({msg: "Movie not found."});
-//         else res.json({message: 'Movie successfully deleted.'});
-//     });
-// });
-
-// router.route('/reviews')
-//     .get((req,res) => {
-//         Reviews.find({}).exec(function(err,reviews) {
-//             if (err) {
-//                 console.log(err);
-
-//             }
-//             res.json ({success: true, reviews: reviews})
-
-//         })
-//     })
-//     .post(authJwtController.isAuthenticated, (req, res) => {
-//         var newReview = new Reviews();
-
-//         Movie.find({title: req.body.title}).exec(function(err,movie) {
-//             if (movie.length != 0)
-//             {
-//                 newReview.movieId = movie[0]._id.toString()
-//                 newReview.username = req.body.username;
-//                 newReview.review = req.body.review;
-//                 newReview.rating = req.body.rating;
-
-//                 newReview.save(function(err) {
-//                     if (err) {
-//                         console.log(err.message);
-//                         return res.json(err);
-//                     }
-
-//                     res.json({success: true, message: 'review created'});
-//                 })
-//             }
-//             else
-//                 res.json({success: false});
-//         });
-//     })
 
 app.use('/', router);
 app.listen(process.env.PORT || 8080);
